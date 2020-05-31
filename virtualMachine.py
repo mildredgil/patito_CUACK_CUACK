c4c40421b8bd5f7282f2a8d84cbeefbe96b1ce1e
Mildred Gil <mi.mitel@hotmail.com>
2020-05-30 19:47:18 -0500

    memory definition vm

--- virtualMachine.py
+++ virtualMachine.py
@@ -2,51 +2,66 @@ import csv
 from quad import Quad
 from dataTable import DirFunc, VarTable
 from decimal import Decimal as D
+from dataStructure import stack
+from memoryConstants import MEM_GLOBAL, MEM_LOCAL, MEM_CONST, CONST_CHAR
+from vmMemory import *
 
-class Memory():
-    
+class CurrentMemory():
     def __init__(self):
-        self.memory =  {
-            0: {},
-            1: {}, 
-            2: {}, 
-            3: {}, 
-            4: {}, 
-            5: {}, 
-            6: {}, 
-            7: {}, 
-            8: {}, 
-            9: {}, 
-            10: {}, 
-            11: {}, 
-            12: {}, 
-            13: {}, 
-            14: {}, 
-            15: {}, 
-            16: {}, 
-            17: {}
-        }
+        self.memory =  [
+            Memory(GLOBAL),
+            Memory(LOCAL),
+            Memory(CONST),
+        ]
+        
+    def insert(self, memory, val):
+        if memory < MEM_LOCAL:
+            self.memory[GLOBAL].insert(memory, val)
+        elif memory < MEM_CONST:
+            self.memory[LOCAL].insert(memory, val)
+        else:
+            self.memory[CONST].insert(memory, val)
+        
+    def value(self, memory):
+        if memory < MEM_LOCAL:
+            return self.memory[GLOBAL].value(memory)
+        elif memory < MEM_CONST:
+            return self.memory[LOCAL].value(memory)
+        else:
+            return self.memory[CONST].value(memory)
+        
+class Memory(): 
+    def __init__(self,scope):
+        self.memory =  memory_struct[scope]
+        self.scope = scope
 
     def insert(self, memory, val):
-        memType = memory // 1000
-        memPos = memory % 1000
-        self.memory[memType][memPos] = val
-
+        memType = memory // 10000
+        memPos = memory % 10000
+        if MEM_INFO[memType][TYPE] == INT:
+            value = int(val)
+        elif MEM_INFO[memType][TYPE] == FLOAT:
+            value = float(val)
+        
+        self.memory[ MEM_INFO[memType][ISTEMP] ][ MEM_INFO[memType][ISPOINTER] ][ MEM_INFO[memType][TYPE] ][ memPos ] = value
+ 
     def value(self, memory):
-        memType = memory // 1000
-        memPos = memory % 1000
-        return self.memory[memType][memPos]
-
-
+        memType = memory // 10000
+        memPos = memory % 10000
+        return self.memory[ MEM_INFO[memType][ISTEMP] ][ MEM_INFO[memType][ISPOINTER] ][ MEM_INFO[memType][TYPE] ][ memPos ]
+        
 class VirtualMachine():
     def __init__(self, filename):
+        #define variables
         self.filename = filename
         self.quad = Quad()
         self.constants = {}
         self.dirFunc = DirFunc()
         self.currentCounter = 0
-        self.memory = Memory()
-        
+        self.pilaMem = stack.Stack()
+        self.currentMem = CurrentMemory()
+
+        #run methods
         self.readFile()
         self.execute()
         
@@ -73,14 +88,16 @@ class VirtualMachine():
                     elif readingContext == 2:
                         self.dirFunc.insert(row['1'],row['2'],row['3'],row['4'],row['5'])
                     else:
-                        if int(row['2']) < 16000:
-                            self.memory.insert(int(row['2']),D(row['1']))
+                        if int(row['2']) < CONST_CHAR:
+                            self.currentMem.insert(int(row['2']),D(row['1']))
                         else:
-                            self.memory.insert(int(row['2']),row['1'])
+                            self.currentMem.insert(int(row['2']),row['1'])
                         
-        #self.quad.print()
-        #self.dirFunc.print()
-        print(self.memory.memory)
+        self.quad.print()
+        self.dirFunc.print()
+        print(self.currentMem.memory[GLOBAL].memory)
+        print(self.currentMem.memory[CONST].memory)
+        #print currentMem
         
     def execute(self):
         instruction = self.quad.get(self.currentCounter)
@@ -101,19 +118,19 @@ class VirtualMachine():
             instruction = self.quad.get(self.currentCounter)
             
     def addAction(self, quad):
-        self.memory.insert(int(quad[3]), self.memory.value(int(quad[1])) + self.memory.value(int(quad[2])))
+        self.currentMem.insert(int(quad[3]), self.currentMem.value(int(quad[1])) + self.currentMem.value(int(quad[2])))
         self.setCounter(self.currentCounter + 1)
 
     def susbtractAction(self, quad):
-        self.memory.insert(int(quad[3]), self.memory.value(int(quad[1])) - self.memory.value(int(quad[2])))
+        self.currentMem.insert(int(quad[3]), self.currentMem.value(int(quad[1])) - self.currentMem.value(int(quad[2])))
         self.setCounter(self.currentCounter + 1)
 
     def timesAction(self, quad):
-        self.memory.insert(int(quad[3]), self.memory.value(int(quad[1])) * self.memory.value(int(quad[2])))
+        self.currentMem.insert(int(quad[3]), self.currentMem.value(int(quad[1])) * self.currentMem.value(int(quad[2])))
         self.setCounter(self.currentCounter + 1)
 
     def divideAction(self, quad):
-        self.memory.insert(int(quad[3]), self.memory.value(int(quad[1])) / self.memory.value(int(quad[2])))
+        self.currentMem.insert(int(quad[3]), self.currentMem.value(int(quad[1])) / self.currentMem.value(int(quad[2])))
         self.setCounter(self.currentCounter + 1)
 
     def printAction(self, quad):
@@ -122,23 +139,23 @@ class VirtualMachine():
         elif quad[1] == ' ':
             print(' ', end="")
         else:
-            print(self.memory.value(int(quad[1])), end ="")
+            print(self.currentMem.value(int(quad[1])), end ="")
         self.setCounter(self.currentCounter + 1)
 
     def readAction(self, quad):
         valor = input("")
-        self.memory.insert(int(quad[3]), valor)
+        self.currentMem.insert(int(quad[3]), valor)
         self.setCounter(self.currentCounter + 1)
 
     def asignAction(self, quad):
-        self.memory.insert(int(quad[3]), self.memory.value(int(quad[1])))
+        self.currentMem.insert(int(quad[3]), self.currentMem.value(int(quad[1])))
         self.setCounter(self.currentCounter + 1)
 
     def gotoAction(self, quad):
         self.setCounter(int(quad[3]))
 
     def gotoFAction(self, quad):
-        if self.memory.value(quad[1]):
+        if self.currentMem.value(quad[1]):
             self.setCounter(self.currentCounter + 1)
         else:
             self.setCounter(int(quad[3]))
---
+++ vmMemory.py
@@ -0,0 +1,140 @@
+#POSITION IN MEM INFO
+SCOPE = 0
+TYPE = 1
+ISTEMP = 2
+ISPOINTER = 3
+
+#TO ACCESS memory structs
+GLOBAL = 0
+LOCAL = 1
+CONST = 2
+
+INT = 0
+FLOAT = 1
+CHAR = 2
+BOOL = 3
+STRING = 3
+
+NO_TEMP = 0
+TEMP = 1
+
+NO_POINTER = 0
+POINTER = 1
+
+MEM_INFO = [  
+    [GLOBAL, NO_TEMP, NO_POINTER, INT],
+    [GLOBAL, NO_TEMP, NO_POINTER, FLOAT],
+    [GLOBAL, NO_TEMP, NO_POINTER, CHAR],
+    [GLOBAL, TEMP,    NO_POINTER, INT],
+    [GLOBAL, TEMP,    NO_POINTER, FLOAT],
+    [GLOBAL, TEMP,    NO_POINTER, CHAR],
+    [GLOBAL, TEMP,    NO_POINTER, BOOL],
+    [GLOBAL, TEMP,    POINTER,    INT],
+    [GLOBAL, TEMP,    POINTER,    FLOAT],
+    [GLOBAL, TEMP,    POINTER,    CHAR],
+    [LOCAL,  NO_TEMP, NO_POINTER,  INT],
+    [LOCAL,  NO_TEMP, NO_POINTER,  FLOAT],
+    [LOCAL,  NO_TEMP, NO_POINTER,  CHAR],
+    [LOCAL,  TEMP,    NO_POINTER,  INT],
+    [LOCAL,  TEMP,    NO_POINTER,  FLOAT],
+    [LOCAL,  TEMP,    NO_POINTER,  CHAR],
+    [LOCAL,  TEMP,    NO_POINTER,  BOOL],
+    [LOCAL,  TEMP,    POINTER,     INT],
+    [LOCAL,  TEMP,    POINTER,     FLOAT],
+    [LOCAL,  TEMP,    POINTER,     CHAR],
+    [CONST,  NO_TEMP, NO_POINTER,  INT],
+    [CONST,  NO_TEMP, NO_POINTER,  FLOAT],
+    [CONST,  NO_TEMP, NO_POINTER,  CHAR],
+    [CONST,  NO_TEMP, NO_POINTER,  STRING]
+]
+
+global_struct = [
+    [
+        [{},{},{}]          #GLOBAL_NO_TEMP_NO_POINTER
+    ],
+    [
+        [{},{},{},{}],      #GLOBAL_TEMP_NO_POINTER
+        [{},{},{}]          #GLOBAL_TEMP_POINTER
+    ],
+]
+
+local_struct = [
+    [
+        [{},{},{}]          #LOCAL_NO_TEMP_NO_POINTER
+    ],
+    [
+        [{},{},{},{}],      #LOCAL_TEMP_NO_POINTER
+        [{},{},{}]          #LOCAL_TEMP_POINTER
+    ],
+]
+
+const_struct = [
+    [
+        [{},{},{},{}],      #CONST_NO_TEMP_NO_POINTER
+    ],
+]
+
+memory_struct = [
+    global_struct,
+    local_struct,
+    const_struct
+]
+
+#delete this later
+'''
+memory_notused = {
+    GLOBAL : {
+        NO_TEMP: {
+            NO_POINTER: {
+                INT: {},
+                FLOAT: {}, 
+                CHAR: {}
+            }
+        },
+        TEMP: {
+            NO_POINTER: {
+            INT: {},
+            FLOAT: {}, 
+            CHAR: {},
+            BOOL: {}
+            },
+            POINTER: {
+                INT: {},
+                FLOAT: {}, 
+                CHAR: {}
+            }
+        }
+    },
+    LOCAL : {
+        NO_TEMP: {
+            NO_POINTER: {
+                INT: {},
+                FLOAT: {}, 
+                CHAR: {}
+            }
+        },
+        TEMP: {
+            NO_POINTER: {
+                INT: {},
+                FLOAT: {}, 
+                CHAR: {},
+                BOOL: {}
+            },
+            POINTER: {
+                INT: {},
+                FLOAT: {}, 
+                CHAR: {}
+            }
+        }
+    },
+    CONST : {
+        NO_TEMP: {
+            NO_POINTER: {
+            INT: {},
+            FLOAT: {}, 
+            CHAR: {}
+            }
+        }
+    }
+}
+'''
